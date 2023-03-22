d41abf17ce86837bfdc58f5ac2bba50b6a5a2e6d
Waldenth <waldenth@outlook.com>
2023-03-22 15:04:05 +0800

    server ver0.2: 用户上线广播, 添加nc客户端程序

--- src/server.go
+++ src/server.go
@@ -3,27 +3,68 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播channel
+	Message chan string
 }
 
 // 创建一个server
 // 以一个大写字母开头,使用这种形式的标识符的对象就可以被外部包的代码所使用
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
 }
 
-// 链接处理业务
+// 监听Message广播channel的goroutine,一旦有消息发送给全部在线User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		// 将msg发送给全部在线user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+// 处理链接业务
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("建立连接成功")
+	//fmt.Println("Connection established successfully.")
+	// 用户上线,onlineMap更新
+	user := NewUser(conn)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	//全局广播消息
+	this.BroadCast(user, " is online now.")
+
+	// 阻塞当前handler
+	select {}
 }
 
 // 启动服务器接口
@@ -35,6 +76,9 @@ func (this *Server) Start() {
 	}
 	//close listen socket
 	defer listener.Close()
+
+	// 启动监听message 的goroutine
+	go this.ListenMessager()
 
 	for {
 		//accept
